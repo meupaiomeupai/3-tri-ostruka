9bb812f676cddc9d359e77e308bce25afea3975e
meupaiomeupai <111287354+meupaiomeupai@users.noreply.github.com>
2024-11-17 22:12:25 -0300

    Update redesSociaisMinhaEscola.js

--- analise-de-dados-aula09/graficos/redesSociaisMinhaEscola.js
+++ analise-de-dados-aula09/graficos/redesSociaisMinhaEscola.js
@@ -1,7 +1,7 @@
 import { criarGrafico, getCSS, incluirTexto } from "./common.js"
 
-async function redesSociaisFavoritasMinhaEscola() {
-    const dadosLocaisString = localStorage.getItem('respostaRedesSociais')
+async function equipamentosAgricolasPreferidos() {
+    const dadosLocaisString = localStorage.getItem('respostaEquipamentosAgricolas')
     if (dadosLocaisString) {
         const dadosLocais = JSON.parse(dadosLocaisString)
         processarDados(dadosLocais)
@@ -9,19 +9,19 @@ async function redesSociaisFavoritasMinhaEscola() {
         const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
         const res = await fetch(url)
         const dados = await res.json()
-        localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
+        localStorage.setItem('respostaEquipamentosAgricolas', JSON.stringify(dados))
         processarDados(dados)
     }
 }
 
 function processarDados(dados) {
-    const redesSociais = dados.slice(1).map(redes => redes[1])
-    const contagemRedesSociais = redesSociais.reduce((acc, redesSociais) => {
-        acc[redesSociais] = (acc[redesSociais] || 0) + 1
+    const tiposEquipamentos = dados.slice(1).map(equipamento => equipamento[1])
+    const contagemEquipamentos = tiposEquipamentos.reduce((acc, tipo) => {
+        acc[tipo] = (acc[tipo] || 0) + 1
         return acc
     }, {})
-    const valores = Object.values(contagemRedesSociais)
-    const labels = Object.keys(contagemRedesSociais)
+    const valores = Object.values(contagemEquipamentos)
+    const labels = Object.keys(contagemEquipamentos)
 
     const data = [
         {
@@ -37,7 +37,7 @@ function processarDados(dados) {
         paper_bgcolor: getCSS('--bg-color'),
         height: 700,
         title: {
-            text: 'Redes sociais que as pessoas da minha escola mais gostam',
+            text: 'Tipos de equipamentos agrícolas preferidos pelos trabalhadores',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
@@ -54,7 +54,7 @@ function processarDados(dados) {
     }
 
     criarGrafico(data, layout)
-    incluirTexto(`Como no mundo, a amostra de pessoas entrevistadas por mim, demonstra um apreço pelo <span>Instagram</span> em relação a outras redes.`)
+    incluirTexto(`Na amostra entrevistada, observamos uma preferência pelo uso de <span>tratores de grande porte</span> para tarefas intensivas, em comparação com outros tipos de equipamentos agrícolas.`)
 }
 
-redesSociaisFavoritasMinhaEscola()
+equipamentosAgricolasPreferidos()
