d1528badb8a43252851b30c49c2f578a1414ed9a
meupaiomeupai <111287354+meupaiomeupai@users.noreply.github.com>
2024-11-17 22:07:10 -0300

    Update informacoesGlobais.js

--- analise-de-dados-aula09/graficos/informacoesGlobais.js
+++ analise-de-dados-aula09/graficos/informacoesGlobais.js
@@ -1,20 +1,20 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-tratadores.json'
 
-async function vizualizarInformacoesGlobais() {
+async function vizualizarInformacoesAgricolas() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+    const totalTratores = (dados.total_tratores / 1e6).toFixed(1)
+    const totalFazendas = (dados.total_fazendas / 1e6).toFixed(1)
+    const horas = parseInt(dados.tempo_medio_uso)
+    const minutos = Math.round((dados.tempo_medio_uso - horas) * 100)
+    const porcentagemUso = ((dados.tratores_em_uso / dados.total_tratores) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `Sabia que existem aproximadamente <span>${totalTratores} milhões</span> de tratores em operação ao redor do mundo, distribuídos em cerca de <span>${totalFazendas} milhões</span> de fazendas? Em média, um trator é utilizado por <span>${horas} horas</span> e <span>${minutos} minutos</span> diariamente.<br>Isso significa que cerca de <span>${porcentagemUso}%</span> dos tratores estão em uso ativo em qualquer momento.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesGlobais()
+vizualizarInformacoesAgricolas()
